24e88719d7910e54095bee699656fc520127ed15
Kevin Mulvey <kmulvey@linux.com>
2023-03-06 09:55:36 -0700

    create cli flags for different modes

--- main.go
+++ main.go
@@ -8,16 +8,19 @@ import (
 	"time"
 
 	log "github.com/sirupsen/logrus"
+	"gorm.io/gorm"
 
 	"go.szostok.io/version"
 	"go.szostok.io/version/printer"
 )
 
+var dateFormat = "2006/01/02 15:04:05"
+
 func main() {
 
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp:   true,
-		TimestampFormat: "2006-01-02 15:04:05",
+		TimestampFormat: dateFormat,
 	})
 
 	var sigChannel = make(chan os.Signal, 1)
@@ -26,9 +29,12 @@ func main() {
 	// get user opts
 	var cmdPath string
 	var pollInterval time.Duration
-	var v bool
+	var v, db, http, prom bool
 	flag.StringVar(&cmdPath, "cmd-path", "/usr/sbin/pwrstat", "absolute path to pwstat command")
 	flag.DurationVar(&pollInterval, "poll-interval", time.Minute, "time interval to gather power stats")
+	flag.BoolVar(&db, "db", false, "print version")
+	flag.BoolVar(&http, "http", false, "print version")
+	flag.BoolVar(&prom, "prom", false, "print version")
 	flag.BoolVar(&v, "version", false, "print version")
 	flag.BoolVar(&v, "v", false, "print version")
 
@@ -43,11 +49,19 @@ func main() {
 		os.Exit(0)
 	}
 
-	var db, err = dbConnect("cp.db")
-	if err != nil {
-		log.Fatal(err)
+	var dbHandle *gorm.DB
+	var err error
+
+	if db {
+		dbHandle, err = dbConnect("cp.db")
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	go webServer(":8080", db)
+
+	if http {
+		go webServer(":8080", dbHandle)
+	}
 
 	var ticker = time.NewTicker(pollInterval)
 	for {
@@ -60,14 +74,21 @@ func main() {
 				log.Fatal(err)
 			}
 
-			status, err := parsePowerStats(out)
+			status, _, err := parsePowerStats(out) // DEVICE
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			if err := insert(db, status); err != nil {
-				log.Fatal(err)
+			if db {
+				if err := insert(dbHandle, status); err != nil {
+					log.Fatal(err)
+				}
 			}
+
+			if prom {
+
+			}
+
 		case <-sigChannel:
 			log.Info("shutting down")
 			return
