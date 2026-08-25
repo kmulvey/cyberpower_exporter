2e4d01816fbc1227520659682f462836c4c17716
Kevin Mulvey <kmulvey@linux.com>
2026-08-25 08:54:53 -0600

    deps and new linter config

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/kmulvey/cyberpower_exporter
 go 1.25.0
 
 require (
-	github.com/prometheus/client_golang v1.23.2
-	github.com/sirupsen/logrus v1.9.4
-	github.com/stretchr/testify v1.11.1
+	github.com/prometheus/client_golang v1.24.1
+	github.com/sirupsen/logrus v1.10.1
+	github.com/stretchr/testify v1.12.0
 	go.szostok.io/version v1.2.0
 )
 
@@ -21,7 +21,6 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/fatih/color v1.19.0 // indirect
 	github.com/goccy/go-yaml v1.19.2 // indirect
@@ -29,24 +28,23 @@ require (
 	github.com/hashicorp/go-version v1.9.0 // indirect
 	github.com/hokaccha/go-prettyjson v0.0.0-20211117102719-0474bc63780f // indirect
 	github.com/huandu/xstrings v1.5.0 // indirect
-	github.com/lucasb-eyer/go-colorful v1.4.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.4.1 // indirect
 	github.com/mattn/go-colorable v0.1.15 // indirect
-	github.com/mattn/go-isatty v0.0.22 // indirect
-	github.com/mattn/go-runewidth v0.0.24 // indirect
+	github.com/mattn/go-isatty v0.0.24 // indirect
+	github.com/mattn/go-runewidth v0.0.28 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
-	github.com/prometheus/common v0.69.0 // indirect
-	github.com/prometheus/procfs v0.20.1 // indirect
+	github.com/prometheus/common v0.70.1 // indirect
+	github.com/prometheus/procfs v0.21.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
-	golang.org/x/crypto v0.53.0 // indirect
-	golang.org/x/sys v0.46.0 // indirect
-	google.golang.org/protobuf v1.36.11 // indirect
+	golang.org/x/crypto v0.55.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+	google.golang.org/protobuf v1.36.12 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- stats.go
+++ stats.go
@@ -5,6 +5,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
+const modelLabel = "model_name"
+
 // nolint: gochecknoglobals
 var (
 	promNamespace = "cyber_power_exporter"
@@ -13,65 +15,65 @@ var (
 		Namespace: promNamespace,
 		Name:      "state",
 		Help:      "0=Normal / 1=Power Failure",
-	}, []string{"model_name"})
+	}, []string{modelLabel})
 
 	powerSuppliedByGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: promNamespace,
 		Name:      "power_supplied_by",
 		Help:      "0=Utility Power / 1=Battery Power",
-	}, []string{"model_name"})
+	}, []string{modelLabel})
 
 	utilityVoltageGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: promNamespace,
 		Name:      "utility_voltage",
 		Help:      "Utility Voltage",
-	}, []string{"model_name"})
+	}, []string{modelLabel})
 
 	outputVoltageGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: promNamespace,
 		Name:      "output_voltage",
 		Help:      "Output Voltage",
-	}, []string{"model_name"})
+	}, []string{modelLabel})
 
 	batteryCapacityGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: promNamespace,
 		Name:      "battery_capacity",
 		Help:      "Battery Capacity as %",
-	}, []string{"model_name"})
+	}, []string{modelLabel})
 
 	remainingRuntimeGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: promNamespace,
 		Name:      "remaining_runtime",
 		Help:      "Remaining Runtime on battery in seconds",
-	}, []string{"model_name"})
+	}, []string{modelLabel})
 
 	loadWattsGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: promNamespace,
 		Name:      "load_watts",
 		Help:      "Current Load in watts",
-	}, []string{"model_name"})
+	}, []string{modelLabel})
 
 	loadPctGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: promNamespace,
 		Name:      "load_pct",
 		Help:      "current load as %",
-	}, []string{"model_name"})
+	}, []string{modelLabel})
 
 	lineInteractionGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: promNamespace,
 		Name:      "line_interaction",
 		Help:      "ups line interaction",
-	}, []string{"model_name"})
+	}, []string{modelLabel})
 
 	testResultGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: promNamespace,
 		Name:      "test_result",
 		Help:      "result of last test result",
-	}, []string{"model_name"})
+	}, []string{modelLabel})
 
 	lastPowerEventDurationGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: promNamespace,
 		Name:      "last_power_event_duration",
 		Help:      "how long the last event lasted",
-	}, []string{"model_name"})
+	}, []string{modelLabel})
 )
